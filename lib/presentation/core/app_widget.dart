85885caf77a3e91c5ce6a3ae5e982a9fac952be2
Virtuoso O <odunayoojomo0@gmail.com>
2022-05-31 19:27:05 +0100

    set up

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:somry/injection.dart';
 import 'package:somry/presentation/routes/router.gr.dart' as router;
 // import 'package:stacked_services/stacked_services.dart';
@@ -11,16 +12,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      routerDelegate: _appRouter.delegate(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      builder: (context, widget) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        routeInformationParser: _appRouter.defaultRouteParser(),
+        routerDelegate: _appRouter.delegate(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        builder: (context, widget) {
+          return MediaQuery(
+            //Setting font does not change with system font size
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: widget!,
+          );
+        },
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:somry/application/home/home_view_model.dart';
+import 'package:stacked/stacked.dart';
+
+import '../../injection.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +15,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      viewModelBuilder: ()=>getIt<HomeViewModel>(),
+
+      builder:(context,model,child)=> Scaffold(
+        body:SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Enter  text for summary here',style:TextStyle(fontSize: 15.sp))
+            ],
+          ),
+        )
+      ));
   }
 }
