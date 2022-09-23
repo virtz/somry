cb91ea591a2c1ebb1488e37016a034a0cc2f94a0
Virtuoso O <odunayoojomo0@gmail.com>
2022-09-23 22:14:19 +0100

    feat: share screen preparation

--- lib/presentation/share_feat/share_screen.dart
+++ lib/presentation/share_feat/share_screen.dart
@@ -2,9 +2,14 @@
 
 import 'package:flutter/material.dart';
 
-class ShareScreen extends StatelessWidget {
+class ShareScreen extends StatefulWidget {
   const ShareScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ShareScreen> createState() => _ShareScreenState();
+}
+
+class _ShareScreenState extends State<ShareScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
