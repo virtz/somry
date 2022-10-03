9c34e3daa5c6a7b78555d646911c8c504287a081
Virtuoso O <odunayoojomo0@gmail.com>
2022-10-03 23:58:09 +0100

    feat: share feature successully added

--- lib/presentation/home/summary_display.dart
+++ lib/presentation/home/summary_display.dart
@@ -22,9 +22,8 @@ class _SummeryDisplayState extends State<SummeryDisplay> {
     return ViewModelBuilder<HomeViewModel>.reactive(
       viewModelBuilder: () => getIt<HomeViewModel>(),
       builder: (context, model, child) => Scaffold(
-          body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15.w),
-        child: SingleChildScrollView(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             SizedBox(height: size.height * 0.08),
@@ -33,15 +32,25 @@ class _SummeryDisplayState extends State<SummeryDisplay> {
             Text('Here\'s your summary...',
                 style: TextStyle(fontSize: 15.5.sp)),
             SizedBox(height: size.height * 0.02),
-            Text(
-              widget.content!.sm_api_content!,
-              textAlign: TextAlign.justify,
-              maxLines: model.readMore ? null : 5,
-              overflow:
-                  model.readMore ? TextOverflow.visible : TextOverflow.ellipsis,
-              style: TextStyle(
-                height: 1.5.sp,
-                color: Colors.grey[600],
+            SizedBox(
+              height: model.readMore ? size.height * 0.7 : size.height * 0.2,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      widget.content!.sm_api_content!,
+                      textAlign: TextAlign.justify,
+                      maxLines: model.readMore ? null : 5,
+                      overflow: model.readMore
+                          ? TextOverflow.visible
+                          : TextOverflow.ellipsis,
+                      style: TextStyle(
+                        height: 1.5.sp,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             SizedBox(height: size.height * 0.02),
@@ -51,22 +60,51 @@ class _SummeryDisplayState extends State<SummeryDisplay> {
                   !model.readMore ? controller.jumpTo(400.h) : null;
                 },
                 child: Text((model.readMore ? "Read less" : "Read more"))),
-            InkWell(
-              onTap: () {
-                model.copySummary(widget.content!.sm_api_content!);
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(model.isCopied ? 'Copied' : 'Copy'),
-                  SizedBox(width: 5.w),
-                  Icon(model.isCopied ? Icons.check : Icons.copy)
-                ],
-              ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(),
+                ),
+                Expanded(
+                    flex: 2,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.r),
+                      child: Material(
+                        child: SizedBox(
+                          height: 30.h,
+                          child: InkWell(
+                            onTap: () {
+                              model.shareContent(
+                                  widget.content!.sm_api_content!, context);
+                            },
+                            child: Icon(
+                              Icons.share,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                InkWell(
+                  onTap: () {
+                    model.copySummary(widget.content!.sm_api_content!);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(model.isCopied ? 'Copied' : 'Copy'),
+                      SizedBox(width: 5.w),
+                      Icon(model.isCopied ? Icons.check : Icons.copy)
+                    ],
+                  ),
+                ),
+              ],
             )
           ]),
         ),
-      )),
+        bottomNavigationBar: BottomAppBar(),
+      ),
     );
   }
 }
